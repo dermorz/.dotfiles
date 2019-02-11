b1b7fc657b3c2efb34998acd20551cb2f30d06e5
Moritz Pein <mail@morz.me>
2019-02-11 10:29:51 +0100

    Add alias gl (fancy git log)

--- zsh/.zsettings/aliases.zsh
+++ zsh/.zsettings/aliases.zsh
@@ -3,3 +3,4 @@ alias av='ansible-vault'
 alias kl='kubelogin --username moritz.pein --password $(pass ldap | head -1)'
 alias vim=nvim
 alias e=nvim
+alias gl="git log --graph --pretty='%C(auto)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
