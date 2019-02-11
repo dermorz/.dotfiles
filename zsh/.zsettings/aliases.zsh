4cdee57771d5598f421906eb4414cc36f46c0e96
Moritz Pein <mail@morz.me>
2019-02-11 11:48:41 +0100

    add alias for jira

--- zsh/.zsettings/aliases.zsh
+++ zsh/.zsettings/aliases.zsh
@@ -4,3 +4,4 @@ alias kl='kubelogin --username moritz.pein --password $(pass ldap | head -1)'
 alias vim=nvim
 alias e=nvim
 alias gl="git log --graph --pretty='%C(auto)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias j="open $JIRA_URL/issue/$(git rev-parse --abbrev-ref HEAD | sed 's/.*\/\([A-Z]*-[0-9]*\).*/\1/')"
