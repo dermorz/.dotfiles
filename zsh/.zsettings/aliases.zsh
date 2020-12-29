9b522a2f01507bcba953a52b55f3eef1401ceb3f
Moritz Bracht <mail@morz.me>
2020-12-29 21:41:51 +0100

    Add some aliases. Mostly for timewarrior

--- zsh/.zsettings/aliases.zsh
+++ zsh/.zsettings/aliases.zsh
@@ -1,8 +1,20 @@
 alias vim='nvim -O'
 alias e='nvim -O'
-alias gl="git log --graph --pretty='%C(auto)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias gl="git log --graph --pretty='%C(auto)%h%Creset %Cgreen[%cs]%C(auto)%d%Creset %s  %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 alias -g G='|grep '
 
 alias ppass="PASSWORD_STORE_DIR=~/.ppass pass"
 
 alias k='kubectl'
+
+alias ls=exa
+alias ll="ls -l"
+alias la="ll -ah"
+
+alias start="timew start"
+alias stop="timew stop"
+alias resume="timew continue"
+alias task="timew summary from 2020-01-01 - now :id"
+alias track="timew track"
+alias day="timew day"
+alias week="timew week"
